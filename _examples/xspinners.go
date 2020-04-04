d346b655e4d4acabfe41545e2930c1b92000e48c
Rob Porter <rjpdesigns@gmail.com>
2020-04-04 22:19:52 +0100

    Update xspinners.go

--- _examples/xspinners.go
+++ _examples/xspinners.go
@@ -8,9 +8,27 @@ import (
 )
 
 func main() {
-	spin := xspinners.New(xspinners.ARROWS1, " TEST")
+	spin := xspinners.New(xspinners.DOTS1, " TEST")
 	fmt.Println(spin)
 	spin.Start()
-	time.Sleep(10 * time.Second)
+	time.Sleep(5 * time.Second)
+	spin.Stop()
+
+	spin = xspinners.New(xspinners.DOTS2, " TEST")
+	fmt.Println(spin)
+	spin.Start()
+	time.Sleep(5 * time.Second)
+	spin.Stop()
+
+	spin = xspinners.New(xspinners.CIRCLE1, " TEST")
+	fmt.Println(spin)
+	spin.Start()
+	time.Sleep(5 * time.Second)
+	spin.Stop()
+
+	spin = xspinners.New(xspinners.ARROWS1, " TEST")
+	fmt.Println(spin)
+	spin.Start()
+	time.Sleep(5 * time.Second)
 	spin.Stop()
 }
