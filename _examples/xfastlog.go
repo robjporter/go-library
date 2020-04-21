ca6e37fee2b1ba3fd1bb7474b65b72f92bb8284a
Rob Porter <rjpdesigns@gmail.com>
2020-04-21 14:01:34 +0100

    Added xfastlog

---
+++ _examples/xfastlog.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/robjporter/go-library/xfastlog"
+)
+
+func main() {
+	var flog FastLog = xfastlog.NewConsoleLogger("fastlog")
+	flog.Info("hello fastlog!!!")
+	flog.Warning("hello fastlog!!!")
+	flog.Notice("hello fastlog!!!")
+	flog.Debug("hello fastlog!!!")
+	flog.Error("hello fastlog!!!")
+	flog.Fatal("hello fastlog!!!")
+
+	var flog2 FastLog = xfastlog.NewRotateLogger("./fastlog", "fastlog", 1024*1024, 5)
+	flog2.Info("hello fastlog!!!")
+	flog2.Warningf("hello fastlog!!!")
+	flog2.Noticef("hello fastlog!!!")
+}
